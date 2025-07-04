a0263c6805ac7d50770d465d2d09793634ff761b
bata_butter <113527212+batabutter@users.noreply.github.com>
2025-07-04 17:52:02 -0400

    added more api calls

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,10 @@ app = express()
 app.use(cors())
 app.use(express.json())
 
+// add a post
+
 app.post("/posts", async (req, res) => {
+
     try {
         const { content, title } = req.body
 
@@ -20,7 +23,124 @@ app.post("/posts", async (req, res) => {
     } catch (error) {
         console.log(error.message)
     }
+
 })
+
+// add a comment 
+
+app.post("/posts/:id/comments", async (req, res) => {
+
+    try {
+        const { id } = req.params
+        const { content } = req.body
+
+        const newComment = await pool.query(
+            "INSERT INTO comments(post_id, content) VALUES($1, $2) RETURNING *",
+            [id, content]
+        )
+
+        res.json(newComment.rows[0])
+    } catch (error) {
+        console.log(error.message)
+    }
+
+})
+
+
+// get all posts
+
+app.get("/posts", async (req, res) => {
+
+    try {
+        const allPosts = await pool.query("SELECT * FROM posts")
+        res.json(allPosts.rows)
+    } catch (error) {
+        console.log(error.message)
+    }
+
+})
+
+// get a specifc post
+
+app.get("/posts/:id", async (req, res) =>{
+
+    try {
+        const { id } = req.params
+        const post = await pool.query("SELECT * FROM posts WHERE post_id = $1",
+            [id]
+        )
+        res.json(post.rows[0])
+    } catch (error) {
+        console.log(error.message)
+    }
+
+})
+
+// get comments from a specific post
+
+app.get("/posts/:id/comments", async (req, res) =>{
+
+    try {
+        const { id } = req.params
+        const comments = await pool.query("SELECT * FROM comments WHERE post_id = $1",
+            [id]
+        )
+        res.json(comments.rows)
+    } catch (error) {
+        console.log(error.message)
+    }
+
+})
+
+// edit a post
+
+
+
+// edit a comment 
+
+// delete a post (also deletes all comments for that post)
+
+app.delete("/posts/:id", async (req, res) => {
+
+    try {
+        const { id } = req.params
+        const deleteComments = await pool.query(
+            "DELETE FROM comments WHERE post_id = $1",
+            [id])    
+        const deletePost = await pool.query(
+            "DELETE FROM posts WHERE post_id = $1",
+            [id])
+        res.json(`Post deleted with id ${id}`)
+    } catch (error) {
+        console.log(error.message)
+    }
+    
+})
+
+// delete a comment
+
+app.delete("/posts/:post_id/comments/:comment_id", async (req, res) => {
+
+    try {
+        const { post_id, comment_id } = req.params
+        const deleteComment = await pool.query(
+            "DELETE FROM comments WHERE post_id = $1 AND comment_id = $2;",
+            [post_id, comment_id])
+        res.json(`Post comment id delete with id ${comment_id} and post_id \
+            ${post_id}`)
+    } catch (error) {
+        console.log(error.message)
+    }
+    
+})
+
+// upvote a post
+
+// downvote a post
+
+// get upvotes from a post
+
+// get downvotes from a post
 
 app.listen(5000, () => {
     console.log("Server started on port 5000")
--- backend/init.sql
+++ backend/init.sql
@@ -1,13 +1,25 @@
-CREATE DATABASE umdforum
-
-CREATE TABLE posts(
-    post_id SERIAL PRIMARY KEY,
-    title   varchar(80),
-    content varchar(80),
-    created_at TIMESTAMP NOW()
-)
+CREATE DATABASE umdforum;
 
 CREATE TABLE accounts(
-    account_id  SERIAL PRIMARY KEY,
-    user_name   varchar(80)
-)
+    account_id      SERIAL PRIMARY KEY,
+    user_name       varchar(80)
+);
+
+CREATE TABLE posts(
+    post_id         SERIAL PRIMARY KEY,
+    title           varchar(80),
+    content         varchar(80),
+    created_at      TIMESTAMP DEFAULT NOW(),
+    upvotes         numeric DEFAULT 0,
+    downvotes       numeric DEFAULT 0
+);
+
+
+CREATE TABLE comments(
+    comment_id      SERIAL PRIMARY KEY,
+    post_id         int NOT NULL,
+    content         varchar(80),
+    upvotes         numeric DEFAULT 0,
+    downvotes       numeric DEFAULT 0,
+    CONSTRAINT fk_post FOREIGN KEY(post_id) REFERENCES posts(post_id)
+);
